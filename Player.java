3cfc2960c1e27b9dacf6527a5f8e24211a6529c8
Anatole Renard <anatole.renard@mrjaffesclass.com>
2023-09-22 12:17:07 -0700

    Do player and War files.

--- Player.java
+++ Player.java
@@ -8,7 +8,7 @@
 public class Player
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private Deck deck;
 
     /**
      * Constructor for objects of class Player
@@ -16,18 +16,5 @@ public class Player
     public Player()
     {
         // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
     }
 }
